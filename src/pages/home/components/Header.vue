a3a717d16a004c9dac587e5bb6dcf12b0c16c7f8
LF <1475304385@qq.com>
2018-09-23 13:20:48 +0800

    header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+<div class="header">
+	<div class="header-left">
+		<span class="iconfont icon-back">&#xe624;</span>
+	</div>
+	<div class="header-input">
+		<span class="iconfont icon-search">&#xe632;</span>
+	城市/景点/游玩主题
+	</div>
+	<div class="header-right">城市
+		<span class="iconfont icon-b">&#xe64a;</span>
+	</div>
+</div>
+</template>
+<script>
+
+export default{
+	name:'homeHeader',
+	
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.header
+		display:flex
+		height:.86rem
+		background-color:$bgColor
+		line-height:0.86rem
+		color:#fff
+		.header-left
+			width:0.64rem
+			float:left
+			.icon-back{
+				font-size:0.4rem
+				text-align:center
+			}
+		.header-input
+			flex:1
+			margin-top:0.12rem
+			height:0.64rem
+			margin-left:0.2rem;
+			background-color:#fff
+			border-radius:0.1rem
+			color:#ccc
+			line-height:0.64rem
+			.icon-search{
+				margin-left:0.1rem
+			}
+		.header-right
+			width:1.24rem
+			float:right
+			text-align:center
+			line-height:0.86rem
+			.icon-b{
+				font-size:0.24rem
+			}
+			
+</style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,9 +1,14 @@
 <template>
-	<div>home</div>
+	<home-header></home-header>
 </template>
 <script>
+
+import HomeHeader from './components/Header.vue'
 export default{
-	name:'home'
+	name:'home',
+	components:{
+		HomeHeader:HomeHeader
+	}
 }
 </script>
 
